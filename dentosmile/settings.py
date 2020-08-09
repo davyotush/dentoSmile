00e0d0f6127be8ac1e090557c1aba7c7af18b9b8
davyotush <davidorare7@gmail.com>
2020-08-09 10:09:07 +0300

    Changed some stuff

--- dentosmile/settings.py
+++ dentosmile/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h0(zbk1zlfe4xkeb1suqfdhq#z3ue#m7q#5yy%!3k=&ec$(7_u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
